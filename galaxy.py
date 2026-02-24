9baaaea360c2a0ed9af299aef1ab62a2899f9497
JoepGaming <spam1@drugmand.nl>
2026-02-24 15:59:06 +0100

    Update galaxy.py
    upgradet to most recent version with bug fixes

--- galaxy.py
+++ galaxy.py
@@ -3,82 +3,106 @@ import numpy as np
 import math as math
 from abc import ABC, abstractmethod
 
-
-# the actual calculation
-
-class galaxy:
-    G = 6.6743*10**-11
+class Galaxy:
+    G = 6.6743*10**-11  # Make gravittional force global available 
     def __init__(self, name, mass, start_x: float = 0, start_y: float = 0):
+        # super init
         self.name = name
         self.mass = mass
         self.x = start_x
         self.y = start_y
         
-class star(galaxy):
+class Star(Galaxy):
+    #  Star class, static object in the galaxy like the sun
     def get_location(self):
+        # get star's location
         return self.x, self.y
     def get_mass(self):
+        # get star's mass
         return self.mass
 
 
-class planet(galaxy):
-    def __init__ (self, name, mass, start_x: float = 0, start_y: float = 0, start_x_speed: float = 0, start_y_speed: float = 0):
-        super().__init__(name,mass,start_x,start_y)
+class Moving_Object(Galaxy): # Class for moving objects in the galaxy like planets, komets, ufo's etc
+    def __init__ (self, name, Star: Star, mass, start_x: float, start_y: float, start_x_speed: float = 0, start_y_speed: float = 0  ):
+        super().__init__(name, mass,start_x,start_y)
+        #  set moving object own attributes
+        self.reference_x, self.reference_y = Star.get_location()
+        self.reference_mass = Star.get_mass()
+        self.reference_name = Star.name
         self.vx = start_x_speed
         self.vy = start_y_speed
         self.x_positions = []
         self.y_positions = []
 
+    def __str__(self) -> str:
+        #  printable definition of the moving object
+        return f"{self.name} {self.mass} orbiting around {self.reference_name}"
+    
+
     @abstractmethod
     def get_location(self, time_interval):
+        # get current location of the planet. Redefine in the subclass
         pass
     
      
-    def set_refrence_star(self, star: star):
-          self.refrence_x = star.x
-          self.refrence_y = star.y
-          self.refrence_mass = star.mass
+    def set_reference_Star(self, Star: Star):
+        #   set data of reference star, this is the star around which the planets circulate
+          self.reference_x, self.reference_y = Star.get_location( )
+          self.reference_mass = Star.get_mass()
 
 
-class planet_circular_orbit(planet):
+  
 
-    def get_location(self, time_interval):
-         self.r_constant
-         self.time += time_interval
-         theta = self.theta0 + self.omega * self.time
-         self.x = self.refrence_x + self.r_constant * np.cos(theta)
-         self.y = self.refrence_y + self.r_constant * np.sin(theta)
-
-         self.x_positions.append(self.x.real)
-         self.y_positions.append(self.y.real)
-
-         return self.x,self.y,self.x_positions,self.y_positions
-        
-    def set_refrence_star(self, star: star):
-          self.refrence_x = star.x
-          self.refrence_y = star.y
-          self.refrence_mass = star.mass
-          self.r_constant = (((self.x-self.refrence_x)**2 + (self.y-self.refrence_y)**2))**0.5
-          self.omega = ((((self.vx)**2 + (self.vy)**2))**0.5) / self.r_constant
-          if self.y == 0:
-              self.y = 1
-          self.theta0 = (np.arctan2((self.y - self.refrence_y), (self.x - self.refrence_x)))
-          self.time = 0
-
-class planet_physical_orbit(planet):
+class Planet_physical_orbit(Moving_Object):
     def get_location( self,time_interval ) :
-         
-        r = (((self.x-self.refrence_x)**2 + (self.y-self.refrence_y)**2))**0.5
-        Fg = galaxy.G*(self.refrence_mass*self.mass)/r**2
+        ###Physical function for orbit
+        r = (((self.x-self.reference_x)**2 + (self.y-self.reference_y)**2))**0.5 #distance between the planet and the star
+        Fg = Galaxy.G*(self.reference_mass*self.mass)/r**2 #gravitational force
         
-        ax = -Fg * (self.x-self.refrence_x) / r / self.mass
+        # Calculate the current x position in time
+        ax = -Fg * (self.x-self.reference_x) / r / self.mass
         self.vx = self.vx + ax    * time_interval
         self.x = self.x + self.vx * time_interval
+        # Store the x postion in a list
         self.x_positions.append(self.x.real)
 
-        ay = -Fg * (self.y-self.refrence_y) / r / self.mass
+        # Calculate the current y position in time
+        ay = -Fg * (self.y-self.reference_y) / r / self.mass
         self.vy = self.vy + ay    * time_interval 
         self.y = self.y + self.vy * time_interval
+         # Store the y postion in a list
         self.y_positions.append(self.y.real)
         
         return self.x,self.y,self.x_positions,self.y_positions
+    
+
+
+class Object_manager:
+    # Class to instantiate the moving objects. 
+    @staticmethod
+    def create_planet_circular_orbit( name, Star: Star, mass, start_x: float, start_y: float, d =9):
+          ### Mathematical fuction for orbit
+          star_x, star_y = Star.get_location()
+          star_mass = Star.get_mass()      
+          r_constant = (((start_x - star_x)**2 + (start_y -star_y)**2))**0.5 #distance between the planet and the star
+          #get needed speed to achieve circular orbit with pyshics calculation
+          vx, vy = Object_manager.get_needed_speed( r_constant, start_x, start_y, star_mass, Star.x, Star.y) 
+          # Create Physical planet for the asked distance to the star
+          Planet = Object_manager.create_planet_physical_orbit(name, Star, mass, start_x, start_y, vx, vy)
+          return Planet
+    
+    @staticmethod
+    def create_planet_physical_orbit ( name, Star: Star, mass, start_x: float, start_y: float, start_x_speed: float = 0, start_y_speed: float = 0  ):
+         # create Physics calculated orbit by speed mass planet, mass star and G force 
+         Planet = Planet_physical_orbit(name, Star, mass, start_x, start_y, start_x_speed , start_y_speed)
+         return Planet
+
+    @staticmethod
+    def get_needed_speed( radius, x , y, reference_mass, reference_x, reference_y):
+        # Calculate speed for needed radius
+        omega = math.sqrt((Galaxy.G * reference_mass)/radius**3) #calculate angular velocity
+        vt = radius * omega #linear tangential velocity
+        theta = np.degrees(np.arctan2((y - reference_y), (x - reference_x))) # signed angle 
+        vx = np.sin(theta) * vt   # Get x speed
+        vy = np.cos(theta)* vt    # Get y speed
+        return vx, vy
