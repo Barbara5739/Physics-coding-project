7b01a816e3dc530cbfde605baadf4223a1b9f394
JoepGaming <spam1@drugmand.nl>
2026-02-27 17:05:45 +0100

    Update working_code.py
    re-added the need for data file because data file was implemented

--- working_code.py
+++ working_code.py
@@ -233,7 +233,7 @@ lines = {}
 def load_data():
     # get planets, aliens, moons
     workdir = Path(__file__).resolve().parent
-    file_path = workdir / "galaxy_objects.csv"
+    file_path = workdir / "data" / "galaxy_objects.csv"
 
     galaxy_objects = pd.read_csv(file_path,  sep=";")
 
